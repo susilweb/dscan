6536578931f4e67607b2b10bd03b9165ffa15796
Sushil Behera <sbehera@mpgbis.com>
2022-09-12 17:08:09 +0530

    change

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,15 @@ export default function Home() {
               streetAddress: "20341 Whitworth Institute 405 N. Whitworth"
             },
             colleague: [
-              "http://www.xyz.edu/students/alicejones.html",
-              "http://www.xyz.edu/students/bobsmith.html"
+              "http://www.dynamicssquare.ca/",
+              "http://www.dynamicssquare.ca/"
             ],
             email: "mailto:jane-doe@xyz.edu",
             image: "janedoe.jpg",
             jobTitle: "Professor",
             name: "Jane Doe",
             telephone: "(425) 123-4567",
-            url: "http://www.janedoe.com"
+            url: "http://www.dynamicssquare.ca/"
       `
     }
   }
